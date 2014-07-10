b902fa15d160396b389d9c6965af6ee784e8f99f
engone nze davy <engonenzedavy@yahoo.com>
2014-07-10 16:01:26 +0200

    added algo for calibration, curve and constant

--- app.js
+++ app.js
@@ -8,150 +8,108 @@ parser.addListener('end', function(result) {
     console.log('Done.');
 });
 
-// var map = [
-// 	ASMod_numAirTempCatMod_C,
-// 	ASMod_tBrickDsDfl_CA,
-// 	ASMod_stOpModeMsk_CA,
-// 	ExhMod_stSwtEomCatMdl_T,
-// 	ASMod_dwMaxExoEngOut_C,
-// 	ExhMod_rEGRThdCalExoLean_C,
-// 	ASMod_swtLamDiffNSCCorOn_C,
-// 	ASMod_dwMaxExoDSOx_X,
-// 	ASMod_dwMaxExoDSOx_Y,
-// 	ASMod_dwMaxExoDSOx_MAP,
-// 	ASMod_dwMaxExoDNOx_X,
-// 	ASMod_dwMaxExoDNOx_Y,
-// 	ASMod_dwMaxExoDNOx_MAP,
-// 	ASMod_facLimDiffNSCCor_X,
-// 	ASMod_facLimDiffNSCCor_CUR,
-// 	ASMod_numPipeCatUs_CA,
-// 	ExhMod_facExhMnfDpnCor0_X, 
-// 	ExhMod_facExhMnfDpnCor0_CUR,
-// 	ExhMod_facExhMnfDpnCor1_X,
-// 	ExhMod_facExhMnfDpnCor1_CUR,
-// 	ExhMod_facExhMnfDpnCor2_X,
-// 	ExhMod_facExhMnfDpnCor2_CUR,
-// 	ASMod_facVelDpnCor0_X,
-// 	ASMod_facVelDpnCor0_CUR,
-// 	ASMod_facVelDpnCor1_X,
-// 	ASMod_facVelDpnCor1_CUR,
-// 	ASMod_facVelDpnCor2_X,
-// 	ASMod_facVelDpnCor2_CUR,
-// 	ASMod_swtSelMaxExoCat_CA,
-// 	ASMod_numSelExoSlpCat_CA,
-// 	ASMod_swtHCLdModOn_CA,
-// 	ASMod_facCalValFl_C,
-// 	ASMod_tiHCModPT1_CA,
-// 	ASMod_swtSelTempHCLdMod_CA,
-// 	ASMod_numSelTempHCLdMod_CA,
-// 	ASMod_numSelTempHCLdSnsr_CA,
-// 	ASMod_mHCLdCatMax_CA,
-// 	ASMod_swtHCLdModDpdCrv_CA,
-// 	ASMod_numCatSpecCrvIn_C,
-// 	ASMod_facHCLdCatTemp0_X,
-// 	ASMod_facHCLdCatTemp0_CUR,
-// 	ASMod_facHCLdCatTemp1_X,
-// 	ASMod_facHCLdCatTemp1_CUR,
-// 	ASMod_facHCLdCatMsFlw0_X,
-// 	ASMod_facHCLdCatMsFlw0_CUR,
-// 	ASMod_facHCLdCatMsFlw1_X,
-// 	ASMod_facHCLdCatMsFlw1_CUR,
-// 	ASMod_facHCLdCatHCMs0_X,
-// 	ASMod_facHCLdCatHCMs0_CUR,
-// 	ASMod_facHCLdCatHCMs1_X,
-// 	ASMod_facHCLdCatHCMs1_CUR,
-// 	ASMod_facHCUnLdCatTemp0_X,
-// 	ASMod_facHCUnLdCatTemp0_CUR,
-// 	ASMod_facHCUnLdCatTemp1_X,
-// 	ASMod_facHCUnLdCatTemp1_CUR,
-// 	ASMod_swtSelTempBrickUs_CA,
-// 	ASMod_numTempSensBrickUs_CA,
-// 	ASMod_mBrick_CA,
-// 	ASMod_arExchExtBrick_CA,
-// 	ASMod_cpEGBrick_X,
-// 	ASMod_cpEGBrick_CUR,
-// 	ASMod_cpCatBrick0_X,
-// 	ASMod_cpCatBrick0_CUR,
-// 	ASMod_cpCatBrick1_X,
-// 	ASMod_cpCatBrick1_CUR,
-// 	ASMod_cpCatBrick2_X,
-// 	ASMod_cpCatBrick2_CUR,
-// 	ASMod_numBrickSpecCrvIn_C,
-// 	ASMod_swtExoBrickCalcOff_C,
-// 	ASMod_numCatOfsExo_C,
-// 	ExhMod_idxBrickTSensFacExo_C,
-// 	ExhMod_idxTSensFacExo_C,
-// 	ASMod_facEffLeanExoTemp0_X,
-// 	ASMod_facEffLeanExoTemp0_CUR,
-// 	ASMod_facEffLeanExoTemp1_X,
-// 	ASMod_facEffLeanExoTemp1_CUR,
-// 	ASMod_facEffLeanExoTemp2_X,
-// 	ASMod_facEffLeanExoTemp2_CUR,
-// 	ASMod_facEffLeanExoMFl_X,
-// 	ASMod_facEffLeanExoMFl_Y,
-// 	ASMod_facEffLeanExoMFl_MAP,
-// 	ASMod_dwMaxExoBrickCat0_X,
-// 	ASMod_dwMaxExoBrickCat0_CUR,
-// 	ASMod_dwMaxExoBrickCat1_X,
-// 	ASMod_dwMaxExoBrickCat1_CUR,
-// 	ASMod_dwMaxExoBrickCat2_X,
-// 	ASMod_dwMaxExoBrickCat2_CUR,
-// 	ASMod_swtOnOfsExo_C,
-// 	ExhMod_facCorOSCExo_X,
-// 	ExhMod_facCorOSCExo_CUR,
-// 	ExhMod_swtLamDpdOSC_C,
-// 	ExhMod_degyOSCExo_C,
-// 	ExhMod_facFrctOSCLdCor_C,
-// 	ExhMod_mRgbth_X,
-// 	ExhMod_mRgbth_CUR,
-// 	ExhMod_facEffOSC_X,
-// 	ExhMod_facEffOSC_Y,
-// 	ExhMod_facEffOSC_MAP,
-// 	ASMod_numCatRefBrick_CA,
-// 	ASMod_swtTempCatModOff_CA,
-// 	ASMod_numTempOxiCatDs_CA,
-// 	ASMod_numTempOxiCatSurf_CA,
-// 	ASMod_tOxiCDfl_CA,
-// 	ASMod_numOxiCat_CA,
-// 	ASMod_dwEnvOxiCDfl_CA,
-// 	ASMod_numTempPFltDs_C,
-// 	ASMod_numTempPFltSurfSim_C,
-// 	ASMod_numPFlt_C,
-// 	ASMod_tPFltDfl_C,
-// 	ASMod_dVolPFltDfl_C,
-// 	ASMod_swtTempCatOut_CA,
-// 	ASMod_numSelTempOutBrick_CA,
-// 	ASMod_numSelTempOutSens_CA,
-// 	ASMod_numSelTempOutSim_CA,
-// 	ASMod_numPipeCatDs_CA,
-// 	ASMod_numPipeCatUs_CA,
-// 	ASMod_numEngTempNOxCor_C,
-// 	ASMod_facLeanNOxTempEng0_X,
-// 	ASMod_facLeanNOxTempEng0_CUR,
-// 	ASMod_facLeanNOxTempEng1_X,
-// 	ASMod_facLeanNOxTempEng1_CUR,
-// 	ASMod_facLeanNOxTempEng2_X,
-// 	ASMod_facLeanNOxTempEng2_CUR,
-// 	ASMod_facLeanNOxTempEG0_X,
-// 	ASMod_facLeanNOxTempEG0_CUR,
-// 	ASMod_facLeanNOxTempEG1_X,
-// 	ASMod_facLeanNOxTempEG1_CUR,
-// 	ASMod_facLeanNOxTempEG2_X,
-// 	ASMod_facLeanNOxTempEG2_CUR,
-// 	ASMod_swtPresMidPFlt_C,
-// 	ASMod_swtStrEGCatDsOff_C,
-// 	ASMod_swtStrPresEGCatUsOff_C
-// ];
+
+var token = {
+	curve: '_CUR',
+	calibration: '_CA',
+	constant: '_C',
+	map: '_MAP'
+}
+
+var actionToken = {
+	curve: function(obj, element) {
+		var values = $(element).parent().next().next().next().next();
+		obj.xArray = [];
+		obj.yArray = [];
+
+		var rows =  $('table tr', $(values));
+		rows.each(function(i, row){
+			if(i%2 === 0){
+				var columns = $('th', $(row));
+				columns.each(function(i, column){
+					obj.xArray.push($(column).text());
+				});
+			}else{
+				var columns = $('td', $(row));
+				columns.each(function(i, column){
+					var value = $(column).text();
+
+					if(value !== 'y' && value !== 'PHY'){
+						obj.yArray.push(value);
+					}
+				});
+			}
+		});
+		return obj;
+	},
+	calibration: function(obj, element) {
+		var values = $(element).parent().next().next().next().next();
+		obj.values = [];
+
+		var rows =  $('table tr', $(values));
+		rows.each(function(i, row){
+			if(i%2 !== 0){
+				var columns = $('td', $(row));
+
+				columns.each(function(i, column){
+					var value = $(column).text();
+
+					if(value !== 'w' && value !== 'PHY'){
+						obj.values.push(value);
+					}
+				});
+			}
+		});
+		return obj;
+	},
+	constant: function(obj, element) {
+		var values = $(element).parent().next();
+
+		debugger;
+		var row =  $('table tr', $(values))[1];
+		var column = $('td', $(row))[2]
+		obj.value = $(column).text().trim();
+		return obj;
+	},
+	map: function(obj, element) {
+	}
+}
 
 var parse = function() {
 	var filename = document.getElementById('file').value;
-
+	var mapArray = [];
 
 	fs.readFile(filename, 'utf-8', function(err, html) {
 		
-		var data = jQuery('a[name]', html);
+		var data = jQuery('h2 > a[name]', html);
 
-		debugger;
+		data.each(function(index, item){
+
+			var obj = {
+				name: $(item).attr('name')
+			}
+
+			if (obj.name.indexOf(token.calibration) > 0) {
+				//mapArray.push(actionToken['calibration'](obj, item));
+				return;
+			}
+
+			if (obj.name.indexOf(token.curve) > 0) {
+				//mapArray.push(actionToken['curve'](obj, item));
+				return;
+			}
+
+			if (obj.name.indexOf(token.constant) > 0) {
+				mapArray.push(actionToken['constant'](obj, item));
+				return;
+			}
+
+
+			if (obj.name.indexOf(token.map) > 0) {
+				//mapArray.push(actionToken['map'](obj, item));
+				return;
+			}
+		});
 		
 	});
 
